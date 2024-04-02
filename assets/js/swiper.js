2d72d83cafc121b385377bcd7a6227f86f0a4857
OxY623 <fuzzydoorproductions0@gmail.com>
2024-04-02 23:59:32 +0300

    all

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,9 +1,41 @@
 if (window.matchMedia("(max-width: 768px)").matches) {
-    const swiper = new Swiper('.block-services__cards', {
-        direction: 'horizontal',
+    const swiper = new Swiper(".mySwiper", {
         loop: true,
+        // slidesPerView: 1.25,
+        slidesPerView: "auto",
+        spaceBetween: 16,
+        slideToClickedSlides: true,
         pagination: {
-            el: '.swiper-pagination',
-        }
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        grubCursor: true,
+        keyboard: {
+            enabled: true,
+            onlyInViewport: true,
+            pageUpDown: true,
+        },
+        mousewheel: {
+            sensitivity: 1,
+        },
+
+        slideOverflow: true,
+        breakpoints: {
+            375: {
+                spaceBetween: 18,
+            },
+            425: {
+                spaceBetween: 20,
+            },
+            500: {
+                spaceBetween: 24,
+            },
+            700: {
+                spaceBetween: 24,
+            },
+
+        },
+        allowTouchMove: true,
+
     });
 }
